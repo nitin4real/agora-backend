26294eab8042a99d12297a6eaafa3c94c8b4fb64
nitin4real <nitin4real.gmail.com>
2024-07-04 15:23:11 +0530

    translation support for 4 languages

--- src/app.ts
+++ src/app.ts
@@ -5,51 +5,45 @@ import https from 'https'
 import http from 'http'
 import fs from 'fs'
 import speech from '@google-cloud/speech'
-import { v2 } from '@google-cloud/translate'
+import { TranslationServiceClient, v2 } from '@google-cloud/translate'
 
-import { Server as SocketServer } from 'socket.io'
+import { Socket, Server as SocketServer } from 'socket.io'
 import { google } from '@google-cloud/speech/build/protos/protos';
 import { } from "@google-cloud/speech"
-import multer from 'multer'
 const appId = '6a0d0b12f6074aad818c99ff9355d444';
 const appCertificate = '7c1ea51799fa4f059dd745ce9f83b31c';
 const app = express();
 const PORT = 3012; // HTTPS typically uses port 443
-const HTTP_SERVER = http.createServer(app);
-const io = new SocketServer(HTTP_SERVER, {
-  cors: {
-    origin: '*'
-  }
-})
+// const HTTP_SERVER = http.createServer(app);
 app.use(cors({
   origin: '*'
 }))
-// app.use((req, res, next) => {
-//   console.log('request recived')
-//   next()
-// })
+app.use((req, res, next) => {
+  console.log('request recived')
+  next()
+})
 
 interface StringMap {
   [key: string]: string;
 }
 const uid_name_pair: StringMap = {}
 
-// app.get('/getToken', (req, res) => {
-//   console.log(req.query)
-//   const userId: string = req.query.userId as string
-//   const uid: string = String(userNameToUid(userId));
-//   const channelName: string = req.query.channelName as string
-//   uid_name_pair[`${uid}`] = userId
-//   console.log('sertting', userId, uid, uid_name_pair)
-//   res.send({ tokens: GenerateTokenForUserID(uid, channelName), appId, uid });
-// });
+app.get('/getToken', (req, res) => {
+  console.log(req.query)
+  const userId: string = req.query.userId as string
+  const uid: string = String(userNameToUid(userId));
+  const channelName: string = req.query.channelName as string
+  uid_name_pair[`${uid}`] = userId
+  console.log('sertting', userId, uid, uid_name_pair)
+  res.send({ tokens: GenerateTokenForUserID(uid, channelName), appId, uid });
+});
 
-// app.get('/getUserName', (req, res) => {
-//   console.log(req.query)
-//   const uid: string = req.query.uid as string
-//   console.log(uid_name_pair)
-//   res.send({ uid, userName: uid_name_pair[`${uid}`] });
-// });
+app.get('/getUserName', (req, res) => {
+  console.log(req.query)
+  const uid: string = req.query.uid as string
+  console.log(uid_name_pair)
+  res.send({ uid, userName: uid_name_pair[`${uid}`] });
+});
 
 
 const options = {
@@ -58,73 +52,171 @@ const options = {
 };
 
 
-// const HTTPS_SERVER = https.createServer(options, app);
-// HTTPS_SERVER.listen(PORT, () => {
-//   console.log(`HTTPS SERVER is running on port ${PORT}`);
-// });
-
 // app.listen('3013', () => {
 //   return console.log(`Express is listening at 3030`);
 // });
-// export const GenerateTokenForUserID = (uid: string, channelName: string = '') => {
-//   const expirationTimeInSeconds = 6000
-//   const currentTimestamp = Math.floor(Date.now() / 1000)
-//   const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
-//   const role = RtcRole.PUBLISHER;
+export const GenerateTokenForUserID = (uid: string, channelName: string = '') => {
+  const expirationTimeInSeconds = 6000
+  const currentTimestamp = Math.floor(Date.now() / 1000)
+  const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
+  const role = RtcRole.PUBLISHER;
 
-//   const rtcToken = RtcTokenBuilder.buildTokenWithUid(
-//     appId,
-//     appCertificate,
-//     channelName,
-//     uid,
-//     role,
-//     expirationTimeInSeconds,
-//     privilegeExpiredTs
-//   );
+  const rtcToken = RtcTokenBuilder.buildTokenWithUid(
+    appId,
+    appCertificate,
+    channelName,
+    uid,
+    role,
+    expirationTimeInSeconds,
+    privilegeExpiredTs
+  );
 
-//   const rtmToken =
-//     RtmTokenBuilder.buildToken(
-//       appId,
-//       appCertificate,
-//       uid,
-//       expirationTimeInSeconds
-//     )
-//   return { rtcToken, rtmToken }
+  const rtmToken =
+    RtmTokenBuilder.buildToken(
+      appId,
+      appCertificate,
+      uid,
+      expirationTimeInSeconds
+    )
+  return { rtcToken, rtmToken }
+}
+
+function userNameToUid(username: string): number {
+  const uniqueNumber = Math.floor(Math.random() * 10000)
+  //here every user should be mapped to a number and be saved in the db for later tally//
+  return uniqueNumber
+}
+
+const pTranslator = new TranslationServiceClient()
+
+const { Translate } = v2
+const translatorService = new Translate()
+// pTranslator.ro({
+//   contents: ['to isko To Nahin Honge Milana chahie'],
+//   targetLanguageCode: 'hi',
+//   mimeType: 'plain/text',
+//   parent: ''
+// } )
+// transliterationConfig: {
+//   enableTransliteration: true    
 // }
 
-// function userNameToUid(username: string): number {
-//   const uniqueNumber = Math.floor(Math.random() * 10000)
-//   //here every user should be mapped to a number and be saved in the db for later tally//
-//   return uniqueNumber
-// }
+const translator = async (text: string, targetLang: string) => {
+}
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('audioStream', async (audioUrl) => {
-    const translatedResponse = await startTranslatorServices(audioUrl)
-    console.log('translatedResponse', translatedResponse)
-  });
+type TranslationData = {
+  UID: string
+  currentText: string
+  language: string
+}
+type Language_Spoken = 'en-us' | 'en-in' | 'en' | 'hi' | 'cmn-Hans-CN'
+type Language_Read = 'en' | 'en' | 'en' | 'hi' | 'zh-CN'
+const languageMap = new Map<Language_Spoken, Language_Read>()
+languageMap.set('en-us', 'en')
+languageMap.set('en-in', 'en')
+languageMap.set('en', 'en')
+languageMap.set('hi', 'hi')
+languageMap.set('cmn-Hans-CN', 'zh-CN')
+
+type ConnectionNode = {
+  socket: Socket
+  languageSpoken: Language_Spoken
+  UID: string
+}
+type ConnectionNodeMap = Map<string, ConnectionNode> // {UID, ConnectionNode}
+
+
+const languageListeners = new Map<Language_Read, ConnectionNodeMap>()
+const translateAndSendToAll = async (textToTranslate: string, targetLang: Language_Read, speakerUID: string) => {
+  // can optimize for eng -> eng translations
+  let [translations] = await translatorService.translate(textToTranslate, targetLang);
+
+  const allListeners = languageListeners.get(targetLang)
+  console.log('target - language', targetLang)
+  console.log('transcription', textToTranslate)
+  console.log('translations', translations)
+  allListeners.forEach((listener) => {
+    const connectionSocket = listener.socket
+    connectionSocket.emit('translationData', translations, speakerUID)
+  })
+}
+
+
+const onTranscribe = (transcribedText: string, speakerUID: string) => {
+  for (let language of languageListeners.keys()) {
+    //for each of these languages translate the transcribed text and send to the respective listeners
+    translateAndSendToAll(transcribedText, language, speakerUID)
+  }
+}
+
+
+const handleSocketConnection = (speakerUID: string, language: Language_Spoken, socket: Socket) => {
+  const languageRead = languageMap.get(language)
+
+  if (!languageListeners.has(languageRead)) {
+    languageListeners.set(languageRead, new Map<string, ConnectionNode>())
+  }
+
+  const connectionNode: ConnectionNode = {
+    socket: socket,
+    languageSpoken: language,
+    UID: speakerUID
+  }
+
+  languageListeners.get(languageRead).set(speakerUID, connectionNode)
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    const languageRead = languageMap.get(language)
+    const allListeners = languageListeners.get(languageRead)
+    allListeners.delete(speakerUID)
+    if (allListeners.size === 0) {
+      //remove the language so it does not gets translated to this langauge again
+      languageListeners.delete(languageRead)
+    }
+    //remove it from the map
+    //remove the map entry if after this there are no more sockets listening to this language
+  })
+
+  socket.on('audioStream', async (data) => {
+    const transcription: string = await startTranslatorServices(data, language)
+    onTranscribe(transcription, speakerUID)
+  })
+}
+
+
+const HTTPS_SERVER = https.createServer(options, app);
+const socketServer = new SocketServer(HTTPS_SERVER, {
+  cors: {
+    origin: '*'
+  }
+})
+
+socketServer.on('connection', (socket) => {
+  const {
+    languageCode,
+    userUid
+  } = socket.handshake.query as {
+    languageCode: Language_Spoken,
+    userUid: string
+  }
+  handleSocketConnection(userUid, languageCode, socket)
 });
 
 const speechClient = new speech.SpeechClient();
 
-const request: google.cloud.speech.v1.IStreamingRecognitionConfig = {
-  config: {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 16000,
-    languageCode: 'hi',
-  }
-};
+// const request: google.cloud.speech.v1.IStreamingRecognitionConfig = {
+//   config: {
+//     encoding: 'LINEAR16',
+//     sampleRateHertz: 16000,
+//     languageCode: 'hi',
+//   }
+// };
 
-const startTranslatorServices = async (audioData) => {
+const startTranslatorServices = async (audioData, language: Language_Spoken) => {
   const config: google.cloud.speech.v1p1beta1.IRecognitionConfig = {
     encoding: 'WEBM_OPUS',
     sampleRateHertz: 48000,
-    languageCode: 'hi',
+    languageCode: language,
   };
 
   const audio = {
@@ -140,24 +232,20 @@ const startTranslatorServices = async (audioData) => {
   const transcription = response.results
     .map(result => result.alternatives[0].transcript)
     .join('\n');
-  const translatedValue = await translator(transcription, 'en')
-  return translatedValue
+  return transcription
 }
 
 
 
-HTTP_SERVER.listen(3013, () => {
-  return console.log(`HTTP SERVER is listening at 3013`);
-})
+// HTTP_SERVER.listen(3013, () => {
+//   return console.log(`HTTP SERVER is listening at 3013`);
+// })
 
 
-const { Translate } = v2
-const translate = new Translate()
 
-const translator = async (text: string, targetLang: string) => {
-  let [translations] = await translate.translate(text, targetLang);
-  return translations
-}
+HTTPS_SERVER.listen(PORT, () => {
+  console.log(`HTTPS SERVER is running on port ${PORT}`);
+});
 
 
 
